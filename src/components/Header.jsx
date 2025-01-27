a220a48985994a308db4387b878d98ac93524f0d
yanisyaniss <6mois1mois@gmail.com>
2025-01-27 08:24:03 +0000

    Ajoute header et home.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <Container>
+        Headersddfssdkjl
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  background: #090b13;
+  height: 70px;
+  `
