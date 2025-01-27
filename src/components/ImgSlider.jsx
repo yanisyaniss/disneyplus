a53690618cb3e1e25b509cc677cab0a1bee329d3
yanisyaniss <6mois1mois@gmail.com>
2025-01-27 14:08:31 +0000

    Mise en place du slide dans la page home.jsx

---
+++ src/components/ImgSlider.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
+
+function ImgSlider() {
+
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    }
+
+    return (
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" />
+            </Wrap>
+        </Carousel>
+    )
+}
+
+export default ImgSlider
+
+const Carousel = styled(Slider)`
+`
+
+const Wrap = styled.div`
+`
