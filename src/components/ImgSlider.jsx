2a2c62cdcd468026c7225a1267be8b48df5854ac
yanisyaniss <6mois1mois@gmail.com>
2025-01-27 14:30:06 +0000

    Mise a jour des sliders et ajoute sur la page principal viewers.jsx

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -12,7 +12,7 @@ function ImgSlider() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: true
     }
 
     return (
@@ -21,7 +21,7 @@ function ImgSlider() {
                 <img src="/images/slider-badging.jpg" />
             </Wrap>
             <Wrap>
-                <img src="/images/slider-badging.jpg" />
+                <img src="/images/slider-badag.jpg" />
             </Wrap>
         </Carousel>
     )
@@ -30,7 +30,38 @@ function ImgSlider() {
 export default ImgSlider
 
 const Carousel = styled(Slider)`
+    margin-top: 20px;
+
+    ul li button::before {
+        font-size: 10px;
+        color: rgb(150, 158, 171);
+    }
+
+    li.slick-active button::before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 
 const Wrap = styled.div`
+    cursor: pointer;
+    img {
+        border: 4px solid transparent;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 300ms;
+
+        &:hover {
+            border: 4px solid rgba(249, 249, 249, 0.8);
+        }
+    }
 `
