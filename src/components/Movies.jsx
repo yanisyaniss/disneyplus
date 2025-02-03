c69e8c0e60831a14bcfe4bacdf144372538180db
yanisyaniss <6mois1mois@gmail.com>
2025-02-03 13:12:32 +0000

    EDIT MOVIES SIZE AND VIEWERS

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,23 +5,20 @@ import { selectMovies } from '../features/movie/movieSlice'
 import { useSelector } from 'react-redux'
 
 function Movies() {
-
   const movies = useSelector(selectMovies);
   console.log("This is movies", movies);
 
   return (
     <Container>
-        <h4>Recommended for you</h4>
+        <Title>Recommended for you</Title>
         <Content>
           { movies && movies.map((movie) => (
             <Wrap key={movie.id}>
               <Link to={`/detail/${movie.id}`}>
-                <img src={movie.cardImg} />
+                <img src={movie.cardImg} alt={movie.title} />
               </Link>
             </Wrap>
-          ))
-          }
-
+          ))}
         </Content>
     </Container>
   )
@@ -29,16 +26,22 @@ function Movies() {
 
 export default Movies
 
-
 const Container = styled.div`
+  padding: 20px;
 `
 
 const Content = styled.div`
   display: grid;
-  grid-gap: 25px;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); 
+  justify-content: center;
 `
-
+const Title = styled.h4`
+  font-size: 17px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color: white;
+`
 
 const Wrap = styled.div`
   border-radius: 10px;
@@ -47,11 +50,15 @@ const Wrap = styled.div`
   border: 3px solid rgba(249, 249, 249, 0.1);
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
   rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  transition: all 0.3s ease-in-out;
+  max-width: 500px; /* Empêche les images de devenir trop grandes */
+  margin: auto;
 
   img {
     width: 100%;
-    height: 100%;
+    height: auto;
     object-fit: cover;
+    display: block;
   }
 
   &:hover {
@@ -60,4 +67,4 @@ const Wrap = styled.div`
     rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     border-color: rgba(249, 249, 249, 0.8);
   }
-    `
+`
--- src/components/Viewers.jsx
+++ src/components/Viewers.jsx
@@ -5,35 +5,32 @@ const Viewers = (props) => {
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="" />
-        <video autoPlay={true} loop={true} playsInline={true}>
+        <video autoPlay loop playsInline>
           <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt="" />
-        <video autoPlay={true} loop={true} playsInline={true}>
+        <video autoPlay loop playsInline>
           <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt="" />
-        <video autoPlay={true} loop={true} playsInline={true}>
+        <video autoPlay loop playsInline>
           <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt="" />
-        <video autoPlay={true} loop={true} playsInline={true}>
+        <video autoPlay loop playsInline>
           <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt="" />
-        <video autoPlay={true} loop={true} playsInline={true}>
-          <source
-            src="/videos/1564676296-national-geographic.mp4"
-            type="video/mp4"
-          />
+        <video autoPlay loop playsInline>
+          <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4" />
         </video>
       </Wrap>
     </Container>
@@ -41,56 +38,55 @@ const Viewers = (props) => {
 };
 
 const Container = styled.div`
-  margin-top: 30px;
-  padding: 30px 0px 26px;
+  margin-top: 20px;
+  padding: 20px 0;
   display: grid;
-  grid-gap: 25px;
-  gap: 25px;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  justify-content: center;
 
   @media (max-width: 768px) {
-    grid-template-columns: repeat(1, minmax(0, 1fr));
+    grid-template-columns: repeat(2, minmax(120px, 1fr));
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(1, minmax(150px, 1fr));
   }
 `;
 
 const Wrap = styled.div`
-  padding-top: 56.25%;
-  border-radius: 10px;
-  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  height: 120px;
+  border-radius: 8px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 16px 20px -10px,
+    rgb(0 0 0 / 73%) 0px 12px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-  border: 3px solid rgba(249, 249, 249, 0.1);
+  transition: all 0.3s ease-in-out;
+  border: 2px solid rgba(249, 249, 249, 0.1);
 
   img {
-    inset: 0px;
-    display: block;
-    height: 100%;
-    object-fit: cover;
-    opacity: 1;
-    position: absolute;
-    transition: opacity 500ms ease-in-out 0s;
     width: 100%;
-    z-index: 1;
+    height: 100%;
+    object-fit: contain;
+    transition: opacity 0.5s ease-in-out;
+    position: absolute;
     top: 0;
+    left: 0;
   }
 
   video {
     width: 100%;
     height: 100%;
     position: absolute;
-    top: 0px;
+    top: 0;
+    left: 0;
     opacity: 0;
-    z-index: 0;
+    transition: opacity 0.5s ease-in-out;
   }
 
   &:hover {
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-
-    transform: scale(1.05);
+    transform: scale(1.08);
     border-color: rgba(249, 249, 249, 0.8);
 
     video {
